13714627af7fc1e4f64d46b2e1e3df96aee88d1b
solvire <stevenjscott@gmail.com>
2015-08-31 21:02:00 -0700

    adding the hypermedia test

---
+++ tests/API/HyperBuilderTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace LeadFerret\Lib\API;
+
+use LeadFerret\Models\Contact;
+
+
+/**
+ * @author solvire <stevenjscott@gmail.com>
+ * @group API
+ * @package API
+ * @namespace LeadFerret\Lib\API
+ */
+class HyperBuilderTest extends  \UserTestCase
+{
+    
+    
+    /**
+     * 
+     */
+    public function testCanBuildHyperMediaArray()
+    {
+        
+    }
+    
+    
+}
