e9b111c986a744878086a1cb70b0aaa360be372e
solvire <stevenjscott@gmail.com>
2015-08-31 21:07:38 -0700

    bootstrapping in the tests

---
+++ tests/ModelTestCase.php
@@ -0,0 +1,6 @@
+<?php
+
+class ModelTestCase extends PHPUnit_Framework_TestCase
+{
+	
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Application Name
+ * Same as repo name
+ */
+if(!defined('APPLICATION_NAME')) define('APPLICATION_NAME','php-hypermedia-api');
+
+require_once realpath( __DIR__ . '/../vendor/autoload.php');
+require_once 'ModelTestCase.php';
